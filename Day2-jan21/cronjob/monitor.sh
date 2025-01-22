34e97206e5dafca0329fe1d92199e38571e25999
sirjula <sirjula875@gmail.com>
2025-01-22 23:03:09 +0545

    Semantic HTML, Flexbox, Grid

--- Day2-jan21/cronjob/monitor.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#List top 5 resource-heavy processes and append to log file
-echo "Top 5 resources"
-ps -eo pid,ppid,%cpu,%mem --sort=-%cpu | head -n 6 >> /home/sirjula/Desktop/Intern/monitor.log
-
-# Get current timestamp
-Timestamp=$(date "+%Y-%m-%d %H:%M:%S")
-echo "Log file created on $Timestamp" >> /home/sirjula/Desktop/Intern/monitor.log
-echo "--------------------------------">> /home/sirjula/Desktop/Intern/monitor.log
